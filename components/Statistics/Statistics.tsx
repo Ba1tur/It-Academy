3db91705f51782a7131af514e5ae68bb02ee152a
Ba1tur <zbajtur@gmail.com>
2023-06-19 17:22:23 +0600

    second

---
+++ components/Statistics/Statistics.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './Statistics.module.scss'
+
+const Statistics = () => {
+	return (
+		<section className='container'>
+			<div className={s.statistics_section}>
+				<div className={s.statistics_block}>
+					<div className={s.statistics}>
+
+					</div>
+				</div>
+			</div>		
+		</section>
+	);
+};
+
+export default Statistics;
